735d73f1116f053d8bbcf58ab6e527aa0fa2dc70
Blake Lee <137326318+blake-lee-38@users.noreply.github.com>
2023-12-10 00:30:36 -0600

    Update settings.py

--- fullstack_music_example/settings.py
+++ fullstack_music_example/settings.py
@@ -19,11 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-um+7di%u6ajl97ahdjcml0q_*suocf(ddk#2=xmp@g2@q64#lx'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
 
